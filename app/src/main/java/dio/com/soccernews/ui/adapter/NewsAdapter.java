d0eebf28d50fa23465aec380ce6195f0f519f839
Flávio Paixão <flavio_pc@outlook.com>
2022-06-03 12:56:07 -0300

    definição das ações dos icones de salvar e compartilhar

--- app/src/main/java/dio/com/soccernews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/dio/com/soccernews/ui/adapter/NewsAdapter.java
@@ -3,6 +3,7 @@ package dio.com.soccernews.ui.adapter;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -16,10 +17,12 @@ import dio.com.soccernews.databinding.NewsItemBinding;
 import dio.com.soccernews.domain.News;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
-    private List<News> news;
+    private final List<News> news;
+    private final View.OnClickListener favoriteListener;
 
-    public NewsAdapter(List<News> news) {
+    public NewsAdapter(List<News> news, View.OnClickListener favoriteListener) {
         this.news = news;
+        this.favoriteListener = favoriteListener;
     }
 
     @NonNull
@@ -35,11 +38,23 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         News news = this.news.get(position);
         holder.binding.tvTitle.setText(news.getTitle());
         holder.binding.tvSubtitle.setText(news.getDescription());
+
         holder.binding.btOpenLink.setOnClickListener(v -> {
             Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData(Uri.parse(news.getLink()));
             holder.itemView.getContext().startActivity(i);
         });
+
+        holder.binding.ivShare.setOnClickListener(v -> {
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_SUBJECT, news.getTitle());
+            i.putExtra(Intent.EXTRA_TEXT, news.getLink());
+            holder.itemView.getContext().startActivity(Intent.createChooser(i, "Compartilhar"));
+        });
+
+        holder.binding.ivFavorite.setOnClickListener(this.favoriteListener);
+
         Picasso.get().load(news.getImage())
                 .into(holder.binding.ivThumbnail);
 
